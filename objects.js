c0b0db3d86323745024e1919700d58ff8a5d7dfa
Abdulwahab <abdulwahababdal@gmail.com>
2024-11-05 11:26:44 +0300

    done

--- objects.js
+++ objects.js
@@ -11,7 +11,12 @@
  * - genre: "Programming"
  */
 function createBook(title, author, publishedYear, genre) {
-  // write your code here...
+  return {
+    title: title,
+    author: author,
+    publishedYear: publishedYear,
+    genre: genre,
+  };
 }
 
 // DO NOT CHANGE THE LINE OF CODE BELOW (you can use it for testing your code)
@@ -25,8 +30,9 @@ const book = createBook();
  *
  * Access the book title using dot-notation, and access the publish year using bracket-notation.
  */
+
 function printBookTitleAndYear(book) {
-  // write your code here...
+  return `${book.title} ${book["publishedYear"]}`;
 }
 
 /**
@@ -37,7 +43,8 @@ function printBookTitleAndYear(book) {
  * - returns the book object with a new `pageCount` property
  */
 function addPageCount(book, pageCount) {
-  // write your code here...
+  book.pageCount = pageCount;
+  return book;
 }
 
 /**
@@ -49,7 +56,8 @@ function addPageCount(book, pageCount) {
  * - returns the book object with a new `ISBN` property
  */
 function addISBN(book, ISBN) {
-  // write your code here...
+  book.ISBN = ISBN;
+  return book;
 }
 
 /**
@@ -61,7 +69,8 @@ function addISBN(book, ISBN) {
  * - returns the book object with the `publishedYear` updates
  */
 function updatePublishedYear(book, newYear) {
-  // write your code here...
+  book.publishedYear = newYear;
+  return book;
 }
 
 /**
@@ -73,7 +82,8 @@ function updatePublishedYear(book, newYear) {
  * - returns the book object with the `author` property changed to an array with BOTH authors
  */
 function addSecondAuthor(book, additionalAuthor) {
-  // write your code here...
+  book.author = [book.author, additionalAuthor];
+  return book;
 }
 
 /**
@@ -89,7 +99,15 @@ function addSecondAuthor(book, additionalAuthor) {
  * - returns the book object with the new review included in the reviews array
  */
 function addReview(book, reviewer, comment) {
-  // write your code here
+  if (Array.isArray(book.reviews)) {
+    book.reviews.push({
+      reviewer: reviewer,
+      comment: comment,
+    });
+  } else {
+    book.reviews = [{ reviewer, comment }];
+  }
+  return book;
 }
 
 module.exports = {
